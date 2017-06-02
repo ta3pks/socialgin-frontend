6555f0536cad55a93e7c906d0e047582ef88aca1
furkan <enterdarkside@gmail.com>
2017-06-03 03:56:44 +0400

    added upload image handler and remove handler

--- app/component/pages/modules/share/upload_image.js
+++ app/component/pages/modules/share/upload_image.js
@@ -1,50 +1,71 @@
 import React from "react";
+import { connect } from "react-redux";
 
+import {addImage, removeImage} from "./../../../../actions/shareActions";
 
+@connect(store=>{
+    return {
+        images : store.Share.images
+    }
+})
 export default class UploadImage extends React.Component {
     constructor(){
         super();
         this.state = {
-            images : []
+            showed_images : []
         }
     }
     fileAdder(e){
       const that = this;
       const files = e.target.files;
       if (!files.length) return;
-      console.log(files)
-      that.setState({images : []}, function(){
-          for(let i = 0; i<files.length; i++){
-              const image = new Image();
-              const reader = new FileReader();
-              reader.onload = (e) => {
-                  let fileList = that.state.images.slice();
-                  fileList.push(e.target.result)
-                  that.setState({images: fileList});
-              };
-              reader.readAsDataURL(files[i]);
-            }
-      })
+      for(let i = 0; i<files.length; i++){
+          that.props.dispatch(addImage(files[i]));
+      }
+      for(let i = 0; i<files.length; i++){
+          const reader = new FileReader();
+          reader.onload = (e) => {
+              let fileList = that.state.showed_images.slice();
+              fileList.push(e.target.result);
+              that.setState({showed_images: fileList});
+          };
+          reader.readAsDataURL(files[i]);
+      }
     }
-    
+    removeImage(e){
+        const that = this;
+        const target = e.currentTarget.dataset.index;
+        that.props.dispatch(removeImage(target));
+        const images = that.state.showed_images.slice();
+        images.splice(target, 1);
+        that.setState({
+            showed_images : images
+        })
+    }
     render() {
         const that = this;
-        console.log(this.state.images)
         return (
             <div className="upload_image animated fadeIn">
-                {(_=>{
-                    let images = []
-                    for(let i =0; i<that.state.images.length; i++){
-                        images.push(<img key={i} src={that.state.images[i]} alt=""/>);
-                    }
-                    return images
-                })()}
-                <input className="dont-show" multiple="multiple" onChange={that.fileAdder.bind(that)} ref="fileField" type="file" accept=".jpg,.jpeg,.png" />
-                <div onClick={(_=>{const input = that.refs.fileField; input.click()}).bind(that)} className="new_image">
+                <div className="image-area">
+                {that.state.showed_images.map((value, key)=>{
+                    return(
+                        <div className="image">
+                            <img key={key} src={value} alt=""/>
+                            <div className="remove-image">
+                                <svg viewBox="0 0 24 24" onClick={that.removeImage.bind(that)} data-index={key}>
+                                    <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+                                </svg>
+                            </div>
+                        </div>
+                    )
+                })}
+                 <div onClick={(_=>{const input = that.refs.fileField; input.click()}).bind(that)} className="new_image">
                    <svg viewBox="0 0 24 24">
                        <path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" />
                     </svg>
                 </div>
+                </div>
+                <input className="dont-show" multiple="multiple" onChange={that.fileAdder.bind(that)} ref="fileField" type="file" accept=".jpg,.jpeg,.png" />
             </div>
         );
     }
