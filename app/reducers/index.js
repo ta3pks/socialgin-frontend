dbfff3ce14304963bccec8a5c3af2b4b31713088
furkan <enterdarkside@gmail.com>
2017-06-03 03:57:50 +0400

    share reducer added

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from "redux";
 import User from "./user";
 import Router from "./router";
 import Modal from "./modal";
+import Share from "./share";
 
 export default combineReducers({
     User,
     Router,
     Modal,
+    Share
 })
