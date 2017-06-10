26fcd0aea3377af0c4b7a0fad218ba60e944e2d6
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-06-10 08:58:50 +0400

    added weekly and daily buttons

--- app/component/pages/calendar.js
+++ app/component/pages/calendar.js
@@ -2,13 +2,17 @@ import React from "react";
 import { connect } from "react-redux";
 
 import Month from "./modules/calendar/month";
+import Week from "./modules/calendar/week";
+import Day from "./modules/calendar/day";
+
 import Language from "./../../language/index";
 
-import {nextMonth, previousMonth} from "./../../actions/calendarActions";
+import {nextMonth, previousMonth, setCalendarType} from "./../../actions/calendarActions";
 
 @connect(store=>{
     return {
-        date : store.Calendar.date
+        date : store.Calendar.date,
+        type : store.Calendar.type
     }
 })
 export default class Calendar extends React.Component {
@@ -20,6 +24,10 @@ export default class Calendar extends React.Component {
   }
   previousMonth() {
     this.props.dispatch(previousMonth())
+  }
+  setCalendarType(e) {
+    const actionType = e.currentTarget.dataset.id;
+    this.props.dispatch(setCalendarType(actionType))
   }
   render() {
     const that = this;
@@ -34,7 +42,26 @@ export default class Calendar extends React.Component {
             <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/>
           </svg>
         </div>
-        <Month/>
+        <div className="calendar-options">
+          <span onClick={that.setCalendarType.bind(that)} className={that.props.type === "month" ? "active" : ""} data-id="month">Monthly</span>
+          <span onClick={that.setCalendarType.bind(that)} className={that.props.type === "week" ? "active" : ""} data-id="week">Weekly</span>
+          <span onClick={that.setCalendarType.bind(that)} className={that.props.type === "day" ? "active" : ""} data-id="day">Daily</span>
+        </div>
+        {(_=>{
+          if(that.props.type === "month"){
+            return <Month />
+          }
+        })()}
+        {(_=>{
+          if(that.props.type === "week"){
+            return <Week />
+          }
+        })()}
+        {(_=>{
+          if(that.props.type === "day"){
+            return <Day />
+          }
+        })()}
       </div>
     );
   }
