b712d5e2c944bca054c2b236639a7e9ad22ca593
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-06-10 14:36:34 +0400

    added example events and added type of calendar

--- app/reducers/calendar.js
+++ app/reducers/calendar.js
@@ -1,7 +1,34 @@
 export default function reducer(state={
     date : new Date(),
-    events : [],
-    type : "month"
+    events : [
+        {
+            date : new Date(2017, 6, 11),
+            title : "6. ayin 11 inde paylas"
+        },
+        {
+            date : new Date(2017, 6, 11, 22),
+            title : "6. ayin 11 inde paylas"
+        },
+        {
+            date : new Date(2017, 6, 11, 18),
+            title : "6. ayin 11 inde paylas"
+        },
+        {
+            date : new Date(2017, 6, 15),
+            title : "6. ayin 15 inde paylas"
+        },
+        {
+            date : new Date(2017, 6, 22),
+            title : "6. ayin 22 inde paylas"
+        },
+        {
+            date : new Date(2017, 7, 3),
+            title : "7. ayin 3 inde paylas"
+        }
+    ],
+    type : "month",
+    weekDate : new Date(),
+    dayDate : new Date()
 }, action){
     switch(action.type){
         case "NEXT_MONTH":{
@@ -16,6 +43,30 @@ export default function reducer(state={
         }
         case "SET_CALENDAR_TYPE":{
             state = {...state, type : action.payload}
+            break;
+        }
+        case "NEXT_WEEK":{
+            var firstDay = new Date(state.weekDate);
+            var nextWeek = new Date(firstDay.getTime() + 6 * 24 * 60 * 60 * 1000);
+            state = {...state, weekDate : nextWeek}
+            break;
+        }
+        case "PREVIOUS_WEEK":{
+            var firstDay = new Date(state.weekDate);
+            var previousWeek = new Date(firstDay.getTime() - 6 * 24 * 60 * 60 * 1000);
+            state = {...state, weekDate : previousWeek}
+            break;
+        }
+        case "NEXT_DAY":{
+            var firstDay = new Date(state.dayDate);
+            var nextDay = new Date(firstDay.getTime() + 1 * 24 * 60 * 60 * 1000);
+            state = {...state, dayDate : nextDay}
+            break;
+        }
+        case "PREVIOUS_DAY":{
+            var firstDay = new Date(state.dayDate);
+            var previousDay = new Date(firstDay.getTime() - 1 * 24 * 60 * 60 * 1000);
+            state = {...state, dayDate : previousDay}
             break;
         }
     }
