8d873f7c9e6808caf260d52c6c05a1735996f52e
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-06-10 08:59:23 +0400

    created week system for calendar

---
+++ app/component/pages/modules/calendar/week.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import Language from "./../../../../language/index";
+
+
+@connect(store=>{
+    return {
+        date : store.Calendar.date,
+        events : store.Calendar.events
+    }
+})
+
+export default class CalendarMonth extends React.Component {
+    constructor(){
+        super();
+    }
+    render() {
+        const that = this;
+        return (
+            <div className="calendar-week animated fadeIn">
+                week
+            </div>
+        );
+    }
+}
