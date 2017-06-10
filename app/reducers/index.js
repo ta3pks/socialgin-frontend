20cceba95f1c6723da9760567066965537fc6115
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-06-10 07:54:12 +0400

    added Calendar reducer to combineReducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -4,10 +4,12 @@ import User from "./user";
 import Router from "./router";
 import Modal from "./modal";
 import Share from "./share";
+import Calendar from "./calendar"
 
 export default combineReducers({
     User,
     Router,
     Modal,
-    Share
+    Share,
+    Calendar
 })
