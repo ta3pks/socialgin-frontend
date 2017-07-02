cffa78aff21d56496b623bb277c09bc302ecdbbe
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-07-02 17:05:57 +0400

    loaded sidebar and topbar aded

--- app/reducers/settings.js
+++ app/reducers/settings.js
@@ -1,9 +1,27 @@
 export default function reducer(state={
-    language: "eng"
+    language: "eng",
+    page_load : {
+        topbar : false,
+        sidebar : false
+    }
 }, action){
     switch(action.type){
         case "SET_LANGUAGE":{
             state = {...state, language : action.payload}
+            break;
+        }
+        case "LOADED_TOPBAR":{
+            state = {...state, page_load : {
+                ...state.page_load,
+                topbar : true
+            }}
+            break;
+        }
+        case "LOADED_SIDEBAR":{
+            state = {...state, page_load : {
+                ...state.page_load,
+                sidebar : true
+            }}
             break;
         }
     }
