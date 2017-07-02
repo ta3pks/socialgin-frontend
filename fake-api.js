5a90437668ee17561ae51e200395c627cfc87ef7
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-07-02 17:00:47 +0400

    fake api yapildi

---
+++ fake-api.js
@@ -0,0 +1,60 @@
+module.exports = {
+    "/authenticate": {
+        success : {
+            data : "123456789"
+        },
+        error : {
+            error : "/authenticate de hata var !"
+        }
+    },
+    "/authorize": {
+        success : {
+            data : "123456789"
+        },
+        error : {
+            error : "/authorize de hata var !"
+        }
+    },
+    "/user/get": {
+        success : {
+            data : {
+                name : "Muhammed Furkan",
+                surname : "AYDIN",
+                profile_picture : "/public/img/default_profile.png"
+            }
+        },
+        error : {
+            error : "/user/get de hata var !"
+        }
+    },
+    "/user/getaccounts": {
+        success : {
+            data : [
+                {
+                    id : "123",
+                    name : "Jone Doe",
+                    surname : "",
+                    profile_picture : "/public/img/default_profile.png",
+                    type : "facebook"
+                },
+                {
+                    id : "1234",
+                    name : "Jone Doe",
+                    surname : "",
+                    profile_picture : "/public/img/default_profile.png",
+                    type : "facebook"
+                },
+                {
+                    id : "12345",
+                    name : "Jone Doe",
+                    surname : "",
+                    profile_picture : "/public/img/default_profile.png",
+                    type : "facebook"
+                }
+            ]
+        },
+        error : {
+            error : "/user/getaccounts de hata var !"
+        }
+    }
+}
