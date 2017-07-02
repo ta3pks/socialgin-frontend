10b06e99b8bd7566bd1119b4dce7d83bd76dd262
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-07-02 17:05:41 +0400

    settings added

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -4,12 +4,14 @@ import User from "./user";
 import Router from "./router";
 import Modal from "./modal";
 import Share from "./share";
-import Calendar from "./calendar"
+import Calendar from "./calendar";
+import Settings from "./settings";
 
 export default combineReducers({
     User,
     Router,
     Modal,
     Share,
-    Calendar
+    Calendar,
+    Settings
 })
