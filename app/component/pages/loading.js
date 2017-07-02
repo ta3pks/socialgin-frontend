99840208a62bb32feba26eedf4cfa9ceabc8b411
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-07-02 17:02:56 +0400

    changed loading style

--- app/component/pages/loading.js
+++ app/component/pages/loading.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Topbar from "./../partials/topbar";
+
 export default class Loading extends React.Component {
     constructor(){
         super();
@@ -8,7 +10,12 @@ export default class Loading extends React.Component {
         const that = this;
         return (
             <div className="loading-page animated fadeIn">
-                 <div class="loader"></div>
+                 <Topbar />
+                 <div className="loading-wrapper">
+                     <div className="loading-logo-wrapper">
+                         <img src="/public/img/logo_icon_maincolor.svg" alt="Logo"/>
+                     </div>
+                 </div>
             </div>
         );
     }
