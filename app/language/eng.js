66f9e0c6b2197e04809475eeea594be826d29839
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-07-03 21:21:45 +0400

    Aylarin kisaltmasi eklendi

--- app/language/eng.js
+++ app/language/eng.js
@@ -1,4 +1,5 @@
 module.exports = {
     monthList: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+    monthListShort: ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
     dayNamesShort : ["Sun", "Mon", "Tues", "Weds", "Thurs", "Fri", "Sat"]
 };
