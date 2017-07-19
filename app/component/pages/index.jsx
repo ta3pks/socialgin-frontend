d4ddad0b7e40ba1f0805e6c81c5166ece40d37d5
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-07-19 15:02:44 +0400

    dosya adi degisti

---
+++ app/component/pages/index.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import ManagementPage from "./management.jsx";
+import CalendarPage from "./calendar.jsx";
+import ReportsPage from "./reports.jsx";
+import NotificationsPage from "./notifications.jsx";
+import ProfilePage from "./profile.jsx";
+
+import AddAccountModal from "./modules/add_account/modal.jsx";
+
+@connect(store=>{
+    return {
+        page : store.Router.page
+    }
+})
+
+export default class Pages extends React.Component {
+    constructor(){
+        super();
+    }
+    render() {
+        const that = this;
+        return (
+            <div className="page-area">
+                {(_=>{
+                   var list = this.props.page;
+                   if(list["Management"].active == true){
+                       return <ManagementPage />
+                   }else if(list["Calendar"].active == true){
+                       return <CalendarPage />
+                   }else if(list["Reports"].active == true){
+                       return <ReportsPage />
+                   }else if(list["Profile"].active == true){
+                       return <ProfilePage />
+                   }else if(list["Groups"].active == true){
+                       return (
+                           <div className="coming_soon">
+                               <h1>Coming Soon</h1>
+                               <span>This page is under construction. Please come back later.</span>
+                            </div>
+                       )
+                   }else if(list["Notifications"].active == true){
+                       return <NotificationsPage />
+                   }
+                })()}
+                <AddAccountModal />
+            </div>
+        );
+    }
+}
