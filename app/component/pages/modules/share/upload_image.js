5ffc91da8e62dd9b817a06eb3debd0f3a0027d52
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-07-20 01:14:35 +0400

    changed image display way

--- app/component/pages/modules/share/upload_image.js
+++ app/component/pages/modules/share/upload_image.js
@@ -16,41 +16,34 @@ export default class UploadImage extends React.Component {
         }
     }
     fileAdder(e){
-      const that = this;
-      const files = e.target.files;
-      if (!files.length) return;
-      for(let i = 0; i<files.length; i++){
-          that.props.dispatch(addImage(files[i]));
-      }
-      for(let i = 0; i<files.length; i++){
-          const reader = new FileReader();
-          reader.onload = (e) => {
-              let fileList = that.state.showed_images.slice();
-              fileList.push(e.target.result);
-              that.setState({showed_images: fileList});
-          };
-          reader.readAsDataURL(files[i]);
-      }
+        const that = this;
+        const files = e.target.files;
+        if (!files.length) return;
+        for(let i = 0; i<files.length; i++){
+            const reader = new FileReader();
+            reader.onload = e=>{
+                that.props.dispatch(addImage({
+                    source : files[i],
+                    image : e.target.result
+                }));
+            };
+            reader.readAsDataURL(files[i]);
+        }
     }
     removeImage(e){
         const that = this;
         const target = e.currentTarget.dataset.index;
         that.props.dispatch(removeImage(target));
-        const images = that.state.showed_images.slice();
-        images.splice(target, 1);
-        that.setState({
-            showed_images : images
-        })
     }
     render() {
         const that = this;
         return (
             <div className="upload_image animated fadeIn">
                 <div className="image-area">
-                {that.state.showed_images.map((value, key)=>{
+                {that.props.images.map((value, key)=>{
                     return(
-                        <div className="image">
-                            <img key={key} src={value} alt=""/>
+                        <div className="image" key={window.keyGenerator()}>
+                            <img src={value.image} alt=""/>
                             <div className="remove-image">
                                 <svg viewBox="0 0 24 24" onClick={that.removeImage.bind(that)} data-index={key}>
                                     <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
