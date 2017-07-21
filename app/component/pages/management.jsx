000d3892f372308916fde73214ac615d191c55ff
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-07-22 00:53:40 +0400

    upload image component deleted and changed management page latout

--- app/component/pages/management.jsx
+++ app/component/pages/management.jsx
@@ -4,9 +4,8 @@ import axios from "axios";
 import notifier from "notifier/js/notifier.js";
 
 import Schedule from "./modules/share/schedule";
-import UploadImage from "./modules/share/upload_image";
 
-import {setText, cleanForm} from "./../../actions/shareActions";
+import {setText, cleanForm, addImage, removeImage, setLink} from "./../../actions/shareActions";
 
 import config from "./../../config";
 
@@ -28,11 +27,13 @@ export default class Management extends React.Component {
             schedule : false,
             upload_image : false,
             share_link : false,
-            share_loading : false
+            share_loading : false,
+            closeUrl : false
         }
     }
     textHandler(e){
         this.props.dispatch(setText(e.currentTarget.value))
+        this.urlify(this.props.text)
     }
     startShare(){
         if(this.state.share_loading) return;
@@ -89,6 +90,38 @@ export default class Management extends React.Component {
             })
         })
     }
+    fileAdder(e){
+        const that = this;
+        const files = e.target.files;
+        if (!files.length) return;
+        for(let i = 0; i<files.length; i++){
+            const reader = new FileReader();
+            reader.onload = e=>{
+                that.props.dispatch(addImage({
+                    source : files[i],
+                    image : e.target.result
+                }));
+            };
+            reader.readAsDataURL(files[i]);
+        }
+    }
+    removeImage(e){
+        const that = this;
+        const target = e.currentTarget.dataset.index;
+        that.props.dispatch(removeImage(target));
+    }
+    urlify(text) {
+        const that = this;
+        var urlRegex = /(https?:\/\/[^\s]+)/g;
+        return text.replace(urlRegex, function(url) {
+            that.props.dispatch(setLink(url))
+            axios.get("http://iframe.ly/api/iframely?url=" + url + "&api_key=3a42e524be039ac6afaf5e").then(data=>{
+                that.setState({
+                    urlIframe : data.data.html ? data.data.html : ""
+                })
+            })
+        })
+    }
     render() {
         const that = this;
         return (
@@ -98,7 +131,7 @@ export default class Management extends React.Component {
                         <textarea value={that.props.text} onChange={that.textHandler.bind(that)} className={that.props.text.length > 0 ? "material-input big full" : "material-input big"} placeholder="Status update"></textarea>
                         <div className="controller"> 
                             <div className="left-bar">
-                                <button onClick={(_=>{if(that.state.share_link){that.setState({share_link : false})};that.setState({upload_image : !that.state.upload_image})}).bind(that)} className={that.state.upload_image ? "btn small active" : "btn small"}>
+                                <button onClick={(_=>{const input = that.refs.fileField; input.click()}).bind(that)} className={that.props.images.length > 0 ? "btn small active" : "btn small"}>
                                     <svg viewBox="0 0 24 24">
                                         <path d="M5,14L8.5,9.5L11,12.5L14.5,8L19,14M20,2H4A2,2 0 0,0 2,4V22L6,18H20A2,2 0 0,0 22,16V4C22,2.89 21.1,2 20,2Z" />
                                     </svg>
@@ -116,6 +149,58 @@ export default class Management extends React.Component {
                                 </button>
                             </div>
                         </div>
+                        {(_=>{
+                            if(that.props.text || that.props.images.length > 0 || that.props.link){
+                                return (
+                                    <div className="first-show">
+                                        <div className="show-header">
+                                            <span>Preview post</span>
+                                        </div>
+                                        {(_=>{
+                                            if(that.props.text){
+                                                return(
+                                                    <div className="content">
+                                                        <div dangerouslySetInnerHTML={{__html: that.props.text.replace(/\r\n|\r|\n/g, " <br>")}}>
+                                                        </div>
+                                                    </div>
+                                                )
+                                            }
+                                        })()}
+                                        {(_=>{
+                                            if(that.props.images.length === 0 && that.props.link && !that.state.closeUrl){
+                                                return (
+                                                    <div className="link-preview">
+                                                    </div>
+                                                )
+                                            }
+                                        })()}
+                                        {(_=>{
+                                            if(that.props.images.length > 0){
+                                                return (
+                                                    <div className="upload_image animated fadeIn">
+                                                        <div className="image-area">
+                                                            {that.props.images.map((value, key)=>{
+                                                                return(
+                                                                    <div className="image" key={window.keyGenerator()}>
+                                                                        <img src={value.image} alt=""/>
+                                                                        <div className="remove-image">
+                                                                            <svg viewBox="0 0 24 24" onClick={that.removeImage.bind(that)} data-index={key}>
+                                                                                <path d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+                                                                            </svg>
+                                                                        </div>
+                                                                    </div>
+                                                                )
+                                                            })}
+                                                        </div>
+                                                    </div>
+                                                )
+                                            }
+                                        })()}
+                                    </div>
+                                )
+                            }
+                        })()}
+                        <input className="dont-show" multiple="multiple" onChange={that.fileAdder.bind(that)} ref="fileField" type="file" accept=".jpg,.jpeg,.png" />
                     </div>
                     <div className="share-settings">
                         <Schedule />
