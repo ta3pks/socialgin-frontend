58335acec7350cfd0fa4d08511e61cd7c5a5475b
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-07-23 12:34:51 +0400

    added cookier and axios.

--- app/component/pages/modules/calendar/day.js
+++ app/component/pages/modules/calendar/day.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
+import axios from "axios"
+import cookier from "../../../../../public/js/cookier";
 
+import Config from "../../../../config"
 import Language from "./../../../../language/index";
-
+import {setEvents} from "../../../../actions/calendarActions";
 
 @connect(store=>{
     return {
         dayDate : store.Calendar.dayDate,
-        events : store.Calendar.events
+        events : store.Calendar.events,
+        accounts : store.User.list
     }
 })
 
@@ -15,9 +19,50 @@ export default class CalendarDay extends React.Component {
     constructor(){
         super();
     }
+    componentWillMount(){
+        const that = this;
+        const startDate = Math.round(new Date(that.props.dayDate.getFullYear(), that.props.dayDate.getMonth(),  that.props.dayDate.getDate(), 0, 0, 0).getTime() / 1000);
+        const endTime = Math.round(new Date(that.props.dayDate.getFullYear(), that.props.dayDate.getMonth(), that.props.dayDate.getDate() + 1, 23, 59, 59) .getTime() / 1000);
+        that.props.setLoader()
+        axios.get(Config.calendar, {
+            params: {
+                start : startDate,
+                end : endTime,
+                token : cookier.parse("token")
+            }
+        }).then(data=>{
+            console.log("Beta : ", data.data);
+            that.props.setLoader()
+            const res = data.data;
+            if(res.error) return swal("Error !", res.error, "error");
+            const eventList = {}
+            for(var i=0; i<res.length; i++){
+                var event = res[i];
+                var timeSegment = new Date(Math.round(event.time * 1000));
+                var timeHandler = timeSegment.getFullYear() + "-" + (timeSegment.getMonth() + 1) + "-" + timeSegment.getDate() + "-" + (timeSegment.getHours() < 10 ? "0" + timeSegment.getHours() : timeSegment.getHours());
+                if(eventList[timeHandler]){
+                    eventList[timeHandler].push(event)
+                }else{
+                    eventList[timeHandler] = [];
+                    eventList[timeHandler].push(event)
+                }
+            }
+            that.props.dispatch(setEvents(eventList))
+        }).catch(err=>{
+            console.log("Beta : ", err);
+            swal("Error !", "Somethings went wrong. Please reload this page and try again.", "error")
+        })
+    }
     render() {
         const that = this;
-        return (
+            if(that.props.loading){
+                return (
+                    <div className="content-loader">
+                        <div className="loader"></div>
+                    </div>
+                )
+            }else{
+                return (
             <div className="calendar-day animated fadeIn">
                 <ol className="day-names">
                     <li>{Language.eng.dayNamesShort[that.props.dayDate.getDay()]}</li>
@@ -25,6 +70,7 @@ export default class CalendarDay extends React.Component {
                 <ol className="event-list">
                     {(_=>{
                         const yansit = []
+                        let day = new Date(that.props.dayDate);
                         for(let i=0; i<24; i++){
                             if(i<10) i = "0" + i;
                             yansit.push(
@@ -35,41 +81,40 @@ export default class CalendarDay extends React.Component {
                             yansit.push(
                                 <li key={window.keyGenerator()} className="events">
                                     {(_=>{
-                                        const evetList = []
-                                        for(let q=0; q<that.props.events.length; q++){
-                                            let thisDate = new Date(that.props.events[q].date);
-                                            let nowDate =  that.props.dayDate;
-                                            if(thisDate.getFullYear() == nowDate.getFullYear() && thisDate.getMonth() == nowDate.getMonth() + 1 && thisDate.getDate() == nowDate.getDate() && thisDate.getHours() == i){
-                                                evetList.push(
-                                                    <div key={window.keyGenerator()} className="event">
-                                                        <svg className="close" viewBox="0 0 24 24">
-                                                            <path d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
-                                                        </svg>
+                                        let event = that.props.events[day.getFullYear() + "-" + (day.getMonth() + 1) + "-" + day.getDate() + "-" + i];
+                                        if(event){
+                                           return event.map(currentDateEvent=>{
+                                               return (
+                                                   <div key={window.keyGenerator()} className="event">
                                                         <div className="event-explanation">
-                                                            {that.props.events[q].title}
-                                                        </div>
-                                                        <div className="event-accounts">
-                                                            <div className="social-balon facebook">
-                                                                <svg viewBox="0 0 24 24">
-                                                                    <path d="M17,2V2H17V6H15C14.31,6 14,6.81 14,7.5V10H14L17,10V14H14V22H10V14H7V10H10V6A4,4 0 0,1 14,2H17Z" />
-                                                                </svg>
-                                                            </div>
-                                                            <div className="social-balon twitter">
-                                                                <svg viewBox="0 0 24 24">
-                                                                    <path d="M22.46,6C21.69,6.35 20.86,6.58 20,6.69C20.88,6.16 21.56,5.32 21.88,4.31C21.05,4.81 20.13,5.16 19.16,5.36C18.37,4.5 17.26,4 16,4C13.65,4 11.73,5.92 11.73,8.29C11.73,8.63 11.77,8.96 11.84,9.27C8.28,9.09 5.11,7.38 3,4.79C2.63,5.42 2.42,6.16 2.42,6.94C2.42,8.43 3.17,9.75 4.33,10.5C3.62,10.5 2.96,10.3 2.38,10C2.38,10 2.38,10 2.38,10.03C2.38,12.11 3.86,13.85 5.82,14.24C5.46,14.34 5.08,14.39 4.69,14.39C4.42,14.39 4.15,14.36 3.89,14.31C4.43,16 6,17.26 7.89,17.29C6.43,18.45 4.58,19.13 2.56,19.13C2.22,19.13 1.88,19.11 1.54,19.07C3.44,20.29 5.7,21 8.12,21C16,21 20.33,14.46 20.33,8.79C20.33,8.6 20.33,8.42 20.32,8.23C21.16,7.63 21.88,6.87 22.46,6Z" />
-                                                                </svg>
-                                                            </div>
-                                                            <div className="social-balon linkedin">
-                                                                <svg viewBox="0 0 24 24">
-                                                                    <path d="M21,21H17V14.25C17,13.19 15.81,12.31 14.75,12.31C13.69,12.31 13,13.19 13,14.25V21H9V9H13V11C13.66,9.93 15.36,9.24 16.5,9.24C19,9.24 21,11.28 21,13.75V21M7,21H3V9H7V21M5,3A2,2 0 0,1 7,5A2,2 0 0,1 5,7A2,2 0 0,1 3,5A2,2 0 0,1 5,3Z" />
-                                                                </svg>
-                                                            </div>
+                                                            {(_=>{
+                                                                return (
+                                                                    <span>
+                                                                        {(_=>{
+                                                                            if(currentDateEvent.text){
+                                                                                return currentDateEvent.text
+                                                                            }else if(currentDateEvent.image_links.length){
+                                                                                return currentDateEvent.image_links.length + " images"
+                                                                            }else if(currentDateEvent.link){
+                                                                                return currentDateEvent.link
+                                                                            }
+                                                                        })()}
+                                                                        {", will be shared on "}
+                                                                        {(_=>{
+                                                                            if(that.props.accounts[currentDateEvent.account_id]){
+                                                                                return that.props.accounts[currentDateEvent.account_id].name + " " + that.props.accounts[currentDateEvent.account_id].surname + "."
+                                                                            }else{
+                                                                                return "An account"
+                                                                            }
+                                                                        })()}
+                                                                    </span>
+                                                                )
+                                                            })()}
                                                         </div>
                                                     </div>
                                                 )
-                                            }
-                                        }
-                                        return evetList;
+                                           })
+                                       }
                                     })()}
                                 </li>
                             )
@@ -79,5 +124,6 @@ export default class CalendarDay extends React.Component {
                 </ol>
             </div>
         );
+            }
     }
 }
