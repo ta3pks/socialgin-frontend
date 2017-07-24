35172cb05d88a9f160b1791297522f6d38d5956b
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-07-24 22:53:30 +0400

    added new style selection

--- app/component/partials/sidebar.js
+++ app/component/partials/sidebar.js
@@ -21,11 +21,11 @@ import cookier from "../../../public/js/cookier";
 import "notifier/css/notifier.css";
 
 const colors = {
-    0 : "rgba(75,192,192,0.4)",
-    1 : "rgba(230,126,34,0.4)",
-    2 : "rgba(46,204,113,0.4)",
-    3 : "rgba(52,152,219,0.4)",
-    4 : "rgba(241,196,15,0.4)"
+    0: "rgba(75,192,192,0.4)",
+    1: "rgba(230,126,34,0.4)",
+    2: "rgba(46,204,113,0.4)",
+    3: "rgba(52,152,219,0.4)",
+    4: "rgba(241,196,15,0.4)"
 }
 
 @connect(store => {
@@ -47,84 +47,227 @@ export default class Sidebar extends React.Component {
     openModal() {
         this.props.dispatch(modalOpen())
     }
-    hexToRgbA(hex){
+    hexToRgbA(hex) {
         var c;
-        if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
-            c= hex.substring(1).split('');
-            if(c.length== 3){
-                c= [c[0], c[0], c[1], c[1], c[2], c[2]];
+        if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+            c = hex.substring(1).split('');
+            if (c.length == 3) {
+                c = [c[0], c[0], c[1], c[1], c[2], c[2]];
             }
-            c= '0x'+c.join('');
-            return 'rgba('+[(c>>16)&255, (c>>8)&255, c&255].join(',')+',.4)';
-        }else{
-            return colors[Math.round(Math.random()* 3)]
+            c = '0x' + c.join('');
+            return 'rgba(' + [(c >> 16) & 255, (c >> 8) & 255, c & 255].join(',') + ',1)';
+        } else {
+            return colors[Math.round(Math.random() * 3)]
         }
+    }
+    getLineChart(targetData) {
+        const that = this;
+        var dt = new Date()
+        dt.setMonth(dt.getMonth() - 1)
+        axios.get(config.graph.facebook.fans, {
+            params: {
+                start: Math.round(dt.getTime() / 1000),
+                token: cookier.parse("token"),
+                id: targetData
+            }
+        }).then(data => {
+            const res = data.data;
+            console.log("Beta : ", res);
+            if (res.error) return swal("Error !", res.error || "Somethings went wrong. Please try again later.", "error");
+            const labels = []
+            const datasetData = []
+            res.map(numbers => {
+                let date = new Date(numbers.end_time);
+                labels.push(date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear())
+                let value = numbers.value ? numbers.value : null
+                datasetData.push(value)
+            })
+            that.props.dispatch({
+                type: "SET_LINE_LABELS",
+                payload: labels
+            })
+            const social_accounts = that.props.accounts[targetData];
+            const lineColor = that.hexToRgbA('#' + Math.floor(Math.random() * 16777215).toString(16));
+            that.props.dispatch({
+                type: "SET_LINE_DATASET",
+                payload: {
+                    user_id: social_accounts.id,
+                    label: social_accounts.name + " " + social_accounts.surname,
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: lineColor,
+                    borderColor: lineColor,
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 1.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: lineColor,
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: lineColor,
+                    pointHoverBorderColor: lineColor,
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 5,
+                    pointHitRadius: 10,
+                    data: datasetData
+                }
+            })
+        }).catch(err => {
+            console.log("Beta : ", err);
+            swal("Error !", err || "Somethings went wrong. Please try again later.", "error");
+        })
+    }
+    getBarChart(targetData) {
+        const that = this;
+        var dt = new Date()
+        dt.setMonth(dt.getMonth() - 1)
+        axios.get(config.graph.facebook.gender_age, {
+            params: {
+                start: Math.round(dt.getTime() / 1000),
+                token: cookier.parse("token"),
+                id: targetData
+            }
+        }).then(data => {
+            const res = data.data;
+            console.log("Beta : ", res);
+            if (res.error) return swal("Error !", res.error || "Somethings went wrong. Please try again later.", "error");
+            const labels = []
+            const manData = [];
+            const womenData = [];
+
+            for(let data in res){
+                if(res.hasOwnProperty(data)){
+                    let dataParser = data.split(".");
+                    let gender = dataParser[0];
+                    let ages = dataParser[1]
+                    if(gender === "F"){
+                        womenData.push(res[data])
+                    }else{
+                        manData.push(res[data]);
+                    }
+                    if(labels.includes(ages) == false){
+                        labels.push(ages)
+                    }
+                }
+            }
+            console.log(manData)
+            that.props.dispatch({
+                type: "SET_BAR_LABELS",
+                payload: labels
+            })
+            const social_accounts = that.props.accounts[targetData];
+            let lineColor = that.hexToRgbA('#' + Math.floor(Math.random() * 16777215).toString(16));
+            that.props.dispatch({
+                type: "SET_BAR_DATASET",
+                payload: {
+                    user_id: social_accounts.id,
+                    label: social_accounts.name + " " + social_accounts.surname + "'s Male Statistics",
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: lineColor,
+                    borderColor: lineColor,
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 1.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: lineColor,
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: lineColor,
+                    pointHoverBorderColor: lineColor,
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 5,
+                    pointHitRadius: 10,
+                    data: manData,
+                }
+            })
+            lineColor = that.hexToRgbA('#' + Math.floor(Math.random() * 16777215).toString(16));
+            that.props.dispatch({
+                type: "SET_BAR_DATASET",
+                payload: {
+                    user_id: social_accounts.id,
+                    label: social_accounts.name + " " + social_accounts.surname + "'s Female Statistics",
+                    fill: false,
+                    lineTension: 0.1,
+                    backgroundColor: lineColor,
+                    borderColor: lineColor,
+                    borderCapStyle: 'butt',
+                    borderDash: [],
+                    borderDashOffset: 1.0,
+                    borderJoinStyle: 'miter',
+                    pointBorderColor: lineColor,
+                    pointBackgroundColor: '#fff',
+                    pointBorderWidth: 1,
+                    pointHoverRadius: 5,
+                    pointHoverBackgroundColor: lineColor,
+                    pointHoverBorderColor: lineColor,
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 5,
+                    pointHitRadius: 10,
+                    data: womenData,
+                }
+            })
+        }).catch(err => {
+            console.log("Beta : ", err);
+            swal("Error !", err || "Somethings went wrong. Please try again later.", "error");
+        })
+    }
+    getCountryInfo(targetData){
+        const that = this;
+        var dt = new Date()
+        dt.setMonth(dt.getMonth() - 1)
+        axios.get(config.graph.facebook.country, {
+            params: {
+                start: Math.round(dt.getTime() / 1000),
+                token: cookier.parse("token"),
+                id: targetData
+            }
+        }).then(data => {
+            const res = data.data;
+            console.log("Beta : ", res);
+            if (res.error) return swal("Error !", res.error || "Somethings went wrong. Please try again later.", "error");
+            const social_accounts = that.props.accounts[targetData];
+            const visualData = [
+                ['Country', 'Popularity']
+            ]
+            for(let country in res){
+                visualData.push([country, res[country]]);
+            }
+            that.props.dispatch({
+                type: "SET_COUNTRY_DATASET",
+                payload: {
+                    user_id: social_accounts.id,
+                    label: social_accounts.name + " " + social_accounts.surname + "'s Fans Statistics by Country",
+                    data: visualData,
+                }
+            })
+        }).catch(err => {
+            console.log("Beta : ", err);
+            swal("Error !", err || "Somethings went wrong. Please try again later.", "error");
+        })
     }
     selectAccount(e) {
         const that = this;
         const targetData = e.currentTarget.dataset.key;
         if (!targetData) return;
         if (that.props.page["Reports"].active && that.props.accounts[targetData].selected === false) {
-            var dt = new Date()
-            dt.setMonth(dt.getMonth() - 1)
-            axios.get(config.graph.facebook.fans, {
-                params: {
-                    start: Math.round(dt.getTime() / 1000),
-                    token: cookier.parse("token"),
-                    id: targetData
-                }
-            }).then(data => {
-                const res = data.data;
-                console.log("Beta : ", res);
-                if (res.error) return swal("Error !", res.error || "Somethings went wrong. Please try again later.", "error");
-                const labels = []
-                const datasetData = []
-                res.map(numbers => {
-                    let date = new Date(numbers.end_time);
-                    labels.push(date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear())
-                    let value = numbers.value ? numbers.value : null
-                    datasetData.push(value)
-                })
-                that.props.dispatch({
-                    type: "SET_LINE_LABELS",
-                    payload: labels
-                })
-                const social_accounts = that.props.accounts[targetData];
-                const lineColor = that.hexToRgbA('#' + Math.floor(Math.random()*16777215).toString(16));
-                that.props.dispatch({
-                    type: "SET_LINE_DATASET",
-                    payload: {
-                        user_id : social_accounts.id,
-                        label: social_accounts.name + " " + social_accounts.surname +"'s Facebook Page Fans",
-                        fill: false,
-                        lineTension: 0.1,
-                        backgroundColor: lineColor,
-                        borderColor: lineColor,
-                        borderCapStyle: 'butt',
-                        borderDash: [],
-                        borderDashOffset: 1.0,
-                        borderJoinStyle: 'miter',
-                        pointBorderColor: lineColor,
-                        pointBackgroundColor: '#fff',
-                        pointBorderWidth: 1,
-                        pointHoverRadius: 5,
-                        pointHoverBackgroundColor: lineColor,
-                        pointHoverBorderColor: lineColor,
-                        pointHoverBorderWidth: 2,
-                        pointRadius: 5,
-                        pointHitRadius: 10,
-                        data: datasetData
-                    }
-                })
-            }).catch(err => {
-                console.log("Beta : ", err);
-                swal("Error !", err || "Somethings went wrong. Please try again later.", "error");
-            })
-        }else if(that.props.page["Reports"].active && that.props.accounts[targetData].selected === true){
-            console.log("teas")
+            that.getLineChart(targetData);
+            that.getBarChart(targetData);
+            that.getCountryInfo(targetData);
+        } else if (that.props.page["Reports"].active && that.props.accounts[targetData].selected === true) {
             that.props.dispatch({
-                type : "REMOVE_LINE_DATASET",
-                payload : targetData
+                type: "REMOVE_LINE_DATASET",
+                payload: targetData
+            })
+            that.props.dispatch({
+                type: "REMOVE_BAR_DATASET",
+                payload: targetData
+            })
+            that.props.dispatch({
+                type: "REMOVE_COUNTRY_DATASET",
+                payload: targetData
             })
         }
         that.props.dispatch(selectAccount(targetData))
