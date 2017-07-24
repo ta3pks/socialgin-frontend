071adb069084cb567673e7e60cbb33adb90d1d38
Muhammed Furkan AYDIN <furkanaydin@turkdeveloper.com>
2017-07-24 22:53:16 +0400

    added line bar carts

--- app/component/pages/reports.jsx
+++ app/component/pages/reports.jsx
@@ -4,7 +4,8 @@ import { connect } from "react-redux";
 
 import config from "../../config";
 import cookier from "../../../public/js/cookier";
-import {Line, HorizontalBar} from 'react-chartjs-2';
+import {Line, Bar} from 'react-chartjs-2';
+import {Chart} from 'react-google-charts';
 
 
 @connect(store=>{
@@ -28,13 +29,50 @@ export default class Reports extends React.Component {
             <div className = "reports-page animated fadeIn"> 
                 {(_ => {
                     if(that.props.reports["Line"].datasets.length > 0){
-                        return <Line data={that.props.reports["Line"]} options={{scales: {yAxes: [{ticks: {stepSize: 10}}]}}}/>
+                        return (
+                            <div className="chart">
+                                <div className="chart-title">
+                                    Fans Statistics
+                                </div>
+                                <div className="chart-area">
+                                    <Line data={that.props.reports["Line"]} options={{scales: {yAxes: [{ticks: {stepSize: 10}}]}}}/>
+                                </div>
+                            </div>
+                        )
                     }
                 })()}
                 {(_ => {
-                    if(that.props.reports["Line"].datasets.length > 0){
-                        return <HorizontalBar data={that.props.reports["Line"]}/>
+                    if(that.props.reports["Bar"].datasets.length > 0){
+                        return (
+                            <div className="chart">
+                                <div className="chart-title">
+                                    Gender Statistics
+                                </div>
+                                <div className="chart-area">
+                                    <Bar data={that.props.reports["Bar"]}/>
+                                </div>
+                            </div>
+                        )
                     }
+                })()}
+                {(_ => {
+                    if(that.props.reports["Country"].datasets.length > 0){
+                        return that.props.reports["Country"].datasets.map(dataset=>{
+                            return (
+                                <div className="chart" key={window.keyGenerator()}>
+                                    <div className="chart-title">
+                                        {dataset.label}
+                                    </div>
+                                    <div className="chart-area">
+                                        <Chart chartType="GeoChart" width={"100%"} height={"100%"} data={dataset.data} options={{}} graph_id={window.keyGenerator()} egend_toggle={true}/>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                })()}
+                {(_=>{
+                    
                 })()}
             </div>
         );
